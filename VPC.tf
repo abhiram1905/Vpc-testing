fbe2bcdf3a52c7b5713feb9586a0449fd1173211
abhiram1905 <127509214+abhiram1905@users.noreply.github.com>
2025-08-21 21:58:32 +0530

    Update VPC.tf

--- VPC.tf
+++ VPC.tf
@@ -1,9 +1,10 @@
-backend "s3" {
-  bucket = "api-lambda-09112024"
-  key    = "terraform.tfstate"
-  region = "us-east-1"
+terraform {
+  backend "s3" {
+    bucket = "api-lambda-09112024"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
-
 
 
 
